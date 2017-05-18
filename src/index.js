a8cb7bf532fe308354e0f4391f4ca473eb07db2a
liuyao5201314 <670652018@qq.com>
2017-05-18 21:56:27 +0800

    use redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
 
+import Hello from './containers/Hello'
+import configureStore from './store/configureStore'
+
+const store = configureStore()
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <Hello />
+  </Provider>,
   document.getElementById('root')
-);
+)
---
+++ src/reducers/userinfo.js
@@ -0,0 +1,14 @@
+import * as actionTypes from '../constants/userinfo'
+
+export default function userinfo(state = {}, action) {
+  switch (action.type) {
+    // 登录
+    case actionTypes.USERINFO_LOGIN:
+      return action.data
+    // 修改城市
+    case actionTypes.UPDATE_CITYNAME:
+      return action.data
+    default:
+      return state
+  }
+}
